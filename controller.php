fa90bfa14c17ca6ffafbebc2d58cb09367115dfe
fvillena <villenafabian@gmail.com>
2018-07-07 22:32:57 -0400

    adding missing semicolon

--- controller.php
+++ controller.php
@@ -1,7 +1,7 @@
 <?php
 header('Content-Type: text/html; charset=utf-8');
 include ("model.php");
-include ("credentials.php")
+include ("credentials.php");
 
 $connection = connectMySQL($server,$user,$password,$db);
 
