cf5b1eaebbc64a59366eeac8d63f154b91c2225d
fvillena <villenafabian@gmail.com>
2018-07-13 23:53:01 -0400

    google analytics integration

--- footer.php
+++ footer.php
@@ -1,2 +1,11 @@
 <hr>
 Desarrollado con <i class="fas fa-heart"></i> y <i class="fas fa-coffee"></i> por Estudiantes del Magíster en Informática Médica de la Universidad de Chile
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-50179084-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-50179084-1');
+</script>
