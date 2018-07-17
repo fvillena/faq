1ad803a0fe0e5870bc21839af08d0895c67a27c8
fvillena <villenafabian@gmail.com>
2018-07-17 17:11:39 -0400

    Now the comments are in a table

--- admin.php
+++ admin.php
@@ -11,15 +11,67 @@
     <?php include ("header.php"); ?>
     <?php if (!isset($_SESSION['user']) || ($_SESSION['user_type'] != 1)) {header('Location: index.php');} ?>
     <h1>Panel de Control</h1>
+
     <h2>Comentarios</h2>
+
     <?php if ($comments == false) {
       echo 'sin comentarios';
     } else {
+      echo '
+      <table id="myTable" class="display table table-responsive">
+        <thead>
+          <th>Usuario</th>
+          <th>Comentario</th>
+          <th>Procedimiento</th>
+          <th>Acciones</th>
+          <th>Fecha</th>
+        </thead>
+      ';
       foreach ($comments as $comment) {
-        echo '<li><abbr title="'.$comment["datetime"].'"><b>'.$comment["name"].'</b></abbr>: '.$comment["comment"].' en <a href="view.php?action=viewEntry&id='.$comment["proc_id"].'">'.$comment["procedimientoExamen"].'</a> | <a href="admin.php?action2=deleteComment&comment_id='.$comment["id"].'">Eliminar</a></li>';
+        echo '
+        <tr>
+          <td><b>'.$comment["name"].'</b></td>
+          <td>'.$comment["comment"].'</td>
+          <td><a href="view.php?action=viewEntry&id='.$comment["proc_id"].'">'.$comment["procedimientoExamen"].'</a></td>
+          <td><a href="admin.php?action2=deleteComment&comment_id='.$comment["id"].'">Eliminar</a></td>
+          <td>'.$comment["datetime"].'</td>
+        </tr>
+        ';
       }
+      echo "</table>";
     }
     ?>
     <?php include ("footer.php"); ?>
+    <script type="text/javascript">
+    $(document).ready( function () {
+  $('#myTable').DataTable({
+    "language": {
+        "sProcessing": "Procesando...",
+        "sLengthMenu": "Mostrar _MENU_ registros",
+        "sZeroRecords": "No se encontraron resultados",
+        "sEmptyTable": "Ningún dato disponible en esta tabla",
+        "sInfo": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+        "sInfoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
+        "sInfoFiltered": "(filtrado de un total de _MAX_ registros)",
+        "sInfoPostFix": "",
+        "sSearch": "Buscar:",
+        "sUrl": "",
+        "sInfoThousands": ",",
+        "sLoadingRecords": "Cargando...",
+        "oPaginate": {
+            "sFirst": "Primero",
+            "sLast": "Último",
+            "sNext": "Siguiente",
+            "sPrevious": "Anterior"
+        },
+        "oAria": {
+            "sSortAscending": ": Activar para ordenar la columna de manera ascendente",
+            "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+        }
+    },
+    bFilter: false
+  });
+} );
+    </script>
   </body>
 </html>
