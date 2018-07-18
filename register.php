6a5926b28b055bf5635f7c9fdf33dfaea239ca08
fvillena <villenafabian@gmail.com>
2018-07-18 10:18:43 -0400

    better design

--- register.php
+++ register.php
@@ -12,9 +12,7 @@
     <?php if (isset($_POST["userAdded"])) {
       echo 'usuario '.$_POST["user"].' creado';
     } ?>
-    <form class="" action="register.php" method="POST">
-      <label for="">Correo Electrónico: </label>
-      <input type="email" name="user" value=""><br>
+    <!-- <form class="" action="register.php" method="POST">
       <label for="">Nombre</label>
       <input type="text" name="name" value=""><br>
       <label for="">Contraseña: </label>
@@ -27,7 +25,33 @@
       <input type="hidden" name="action" value="addUser">
       <input type="hidden" name="userAdded" value="true">
       <input type="submit" name="" value="Agregar">
-    </form>
+    </form> -->
+    <form class="" action="register.php" method="POST">
+  <div class="form-group">
+    <label for="exampleInputEmail1">Correo Electrónico</label>
+    <input type="email" name="user" class="form-control" id="exampleInputEmail1" placeholder="Ingrese Correo Electrónico">
+    <small id="emailHelp" class="form-text text-muted">Por favor utilice el correo electrónico institucional.</small>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputName1">Nombre</label>
+    <input type="text" name="name" class="form-control" id="exampleInputName1" placeholder="Ingrese Nombre del Usuario">
+    <small id="nameHelp" class="form-text text-muted">Por favor ingrese nombre y apellido del usuario.</small>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Contraseña</label>
+    <input name="password" type="password" class="form-control" id="exampleInputPassword1" placeholder="Contraseña">
+  </div>
+  <div class="form-group">
+    <label for="exampleInputType1">Tipo de Usuario</label>
+    <select class="form-control" name="type">
+      <option value="1">Administrador</option>
+      <option value="2">Ejecutiva</option>
+    </select><br>
+    <input type="hidden" name="action" value="addUser">
+    <input type="hidden" name="userAdded" value="true">
+  </div>
+  <button type="submit" class="btn btn-primary">Agregar</button>
+</form>
     <?php include ("footer.php") ?>
   </body>
 </html>
