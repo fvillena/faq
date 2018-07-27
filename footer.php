aa82e61e33d548608320d6c9d3f26c36f6e331a7
fvillena <villenafabian@gmail.com>
2018-07-27 14:08:31 -0400

    just aesthetic changes

--- footer.php
+++ footer.php
@@ -1,5 +1,8 @@
-<hr>
-Desarrollado con <i class="fas fa-heart"></i> y <i class="fas fa-coffee"></i> por Estudiantes del Magíster en Informática Médica de la Universidad de Chile
+<br>
+<div class="footer">
+  <br>
+  Desarrollado con <i class="fas fa-heart"></i> y <i class="fas fa-coffee"></i> por Estudiantes del Magíster en Informática Médica de la Universidad de Chile
+</div>
 <!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-50179084-1"></script>
 <script>
--- header.php
+++ header.php
@@ -11,7 +11,10 @@
 <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
 <!-- <link rel="stylesheet" href="//cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css"> -->
 <link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css">
-
+<link rel="stylesheet" href="index.css">
+<div class="header">
+  <img src="https://www.redclinica.cl/Portals/_default/Skins/Skin_HCUCH_17_03/images/logo_hcuch_alfa.png" alt="">
+</div>
 <ul class="nav nav-tabs">
   <li class="nav-item">
     <!-- <a class="nav-link <?php if ($filename == "index") {  echo 'active';} ?>" href="index.php">Inicio</a> -->
